23446ad9ecaaa0140b24a04737b6ce4313c06dab
arya-1808 <shindearya0001@gmail.com>
2026-07-04 19:48:06 +0530

    Add nested loops to print numbers 1 to 3

---
+++ 04 june/pat3.py
@@ -0,0 +1,9 @@
+for i in range(1,4):
+    for j in range(1,i+1):
+        if i==1:
+            print("1", end='')
+        elif i==2:
+            print("2", end='')
+        else:
+            print("3", end='')
+    print()
