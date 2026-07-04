aa73d519b833a1dd333187164e6ce6e739dccba5
arya-1808 <shindearya0001@gmail.com>
2026-07-04 15:10:10 +0530

    Delete 02 june directory

--- 02 june/even.py
+++
@@ -1,8 +0,0 @@
-i=1
-sum=0
-while i<=50:
-    if i%2==0:
-        sum=sum+i
-        print(i)
-    i+=1
-print("sum=",sum)
--- 02 june/sq.py
+++
@@ -1,5 +0,0 @@
-i=11
-while i<=20:
-    sq=i*i
-    print(i,"=",sq)
-    i+=1
