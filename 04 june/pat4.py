1df21f06fc456d5699d81de1ff026e34ecf46dda
arya-1808 <shindearya0001@gmail.com>
2026-07-04 19:48:56 +0530

    Add pattern printing logic in pat4.py

---
+++ 04 june/pat4.py
@@ -0,0 +1,9 @@
+for i in range(3,0,-1):
+    for j in range(1,i+1):
+        if i==3:
+            print("a", end='')
+        elif i==2:
+            print("b", end='')
+        else:
+            print("c", end='')
+    print()
